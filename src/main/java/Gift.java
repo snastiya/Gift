22339753a9b0254ae87ff488bafaf880d9ba3394
Anastasiya Sudarenko <snastiya@mail.ru>
2018-03-12 10:28:00 +0300

    1.0

--- src/main/java/Gift.java
+++ src/main/java/Gift.java
@@ -14,21 +14,24 @@ public class Gift {
         Jellybean jellybean2 = new Jellybean("Мармелад Червячки", 60, 50, "Апельсин");
         Marshmallow marshmallow1 = new Marshmallow("Бон-пари", 50, 100, "Белый");
         Marshmallow marshmallow2 = new Marshmallow("Харибо", 100, 150, "Красный");
-        Stuff [] gift = {candy1, candy2, jellybean1, jellybean2, marshmallow1, marshmallow2};
-        for (Stuff someStuff: gift) {
+        Stuff[] gift = {candy1, candy2, jellybean1, jellybean2, marshmallow1, marshmallow2};
+
+        for (Stuff someStuff : gift) {
+
             System.out.println(someStuff.toString());
 
-            }
+        }
+
         int total_price = 0;
         int total_weight = 0;
 
-        for(Stuff someStuff: gift) {total_price = total_price + someStuff.price;
+        for (Stuff someStuff : gift) {
+            total_price = total_price + someStuff.price;
             total_weight = total_weight + someStuff.weight;
-            System.out.println("Общая стоимость подарка = " + total_price);
-            System.out.println("Общий вес подарка = " + total_weight);
-        }
-        }
 
         }
+        System.out.println("Общая стоимость подарка = " + total_price);
+        System.out.println("Общий вес подарка = " + total_weight);
+    }
 
-
+}
